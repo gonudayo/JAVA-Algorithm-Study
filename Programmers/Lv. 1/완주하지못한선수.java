944c171a02a3269254fcd03f279fa0db7c5dd391
gonudayo <kgw010119@gmail.com>
2021-08-12 23:32:45 +0900

    docs: Add Source Code File

---
+++ Programmers/Lv. 1/완주하지못한선수.java
@@ -0,0 +1,25 @@
+/*
+https://velog.io/@gonudayo/프로그래머스-완주하지-못한-선수/
+*/
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String, Integer> HM = new HashMap<>();
+        
+        for (String name : participant) HM.put(name, HM.getOrDefault(name, 0) + 1);
+        
+        for (String name : completion) HM.put(name, HM.get(name) - 1);
+        
+        for (String key : HM.keySet()) {
+            if (HM.get(key) != 0) {
+                answer = key; 
+                break; 
+            } 
+        } 
+        
+        return answer;
+    }
+}
---
+++ Programmers/Lv. 2/전화번호목록.java
@@ -0,0 +1,21 @@
+/*
+https://velog.io/@gonudayo/Java-알고리즘-프로그래머스-전화번호-목록/
+*/
+import java.util.HashMap;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> HM = new HashMap<>();
+        for (String number : phone_book) HM.put(number, 1);
+        
+        for(int i = 0; i < phone_book.length; i++) {
+            for(int j = 1; j < phone_book[i].length(); j++) {
+                if(HM.containsKey(phone_book[i].substring(0,j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
