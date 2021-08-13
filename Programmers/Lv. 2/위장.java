2e52b9fca70c08c8e21dbad6ad314c0d74832505
gonudayo <kgw010119@gmail.com>
2021-08-13 23:23:46 +0900

    docs: Add Source Code File

---
+++ Programmers/Lv. 2/위장.java
@@ -0,0 +1,21 @@
+/*
+https://velog.io/@gonudayo/Java-알고리즘-프로그래머스-위장/
+*/
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> HM = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++) {
+             HM.put(clothes[i][1], HM.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        for(String key : HM.keySet()) {
+            answer *= HM.get(key) +1;
+        } 
+            
+        return answer - 1;
+    }
+}
